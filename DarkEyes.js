57e65d0823c04554c1eab7181e558a4cb560766b
Karituo-Luster <55764170+Karituo-Luster@users.noreply.github.com>
2022-11-20 03:13:37 -0600

    Update DarkEyes.js

--- DarkEyes.js
+++ DarkEyes.js
@@ -1,5 +1,5 @@
 const axios = require('axios');
-const URL = "http://tommyh.space"
+const URL = "http://tommyh.space";
 const getMetadata = () => {
 	axios
 		.get(URL)
@@ -10,48 +10,40 @@ const getMetadata = () => {
 			console.error(error);
 		});
 };
-getMetadata()
+getMetadata();
 /*
-Step 1: Extract elements of a website as input in HTML format?
+	Step 1: Extract elements of a website as input in HTML format?
+*//*
+	Step 2: Collect & Convert all colors into RGB values; puts them into an array
+*//*
+     This function will search throughout 
+     all inline CSS and embedded stylesheets 
+     to search for all added colors and prints
+     out the list in RGB format
 */
-/*
-Step 2: Collect & Convert all colors into RGB values; puts them into an array
-*/
-	function getAllColors() {
-    	// regex via http://stackoverflow.com/a/7543829/149636
-    	let rgbRegex = /^rgba?\((\d+),\s*(\d+),\s*(\d+)(?:,\s*(\d+(?:\.\d+)?))?\)$/;
-
-    	let allColors = [];
-
-    	let elems = document.getElementsByTagName('*');
-    	let total = elems.length;
-
-    	let x,y,elemStyles,styleName,styleValue,rgbVal;
-
-    	for(x = 0; x < total; x++) {
-     	elemStyles = window.getComputedStyle(elems[x]);
-		for(y = 0; y < elemStyles.length; y++) {
-          	styleName = elemStyles[y];
-          	styleValue = elemStyles[styleName];
-            	if(!styleValue) {
-          		continue;
-          	}
-            // convert to string to avoid match exceptions
-            styleValue += "";
-            rgbVal = styleValue.match(rgbRegex);
-            if(!rgbVal) { // property does not contain a color
-                continue;
-            }
-
-            if(allColors.indexOf(rgbVal.input) == -1) { // avoid duplicate entries
-                allColors.push(rgbVal.input);
-            }
-
-        }
-
-    }
-
-    return allColors;
+let  colorFunction = () => {
+     // regex via http://stackoverflow.com/a/7543829/149636
+     let rgbRegex = /^rgba?\((\d+),\s*(\d+),\s*(\d+)(?:,\s*(\d+(?:\.\d+)?))?\)$/;
+     let colorList = [];
+     let elements = document.getElementsByTagName('*');
+     let total = elements.length;
+     let elemStyles, cssName, cssValue, rgbVal;
+     for(let x = 0; x < total; x++){
+          elemStyles = window.getComputedStyle(elems[x]);
+          for(let y = 0; y < elemStyles.length; y++){
+               cssName = elemStyles[y];
+               cssValue = elemStyles[cssName];
+               if(!cssValue)
+                    continue;
+               cssValue += "";                              // convert to string to avoid match exceptions
+               rgbVal = cssValue.match(rgbRegex);
+               if(!rgbVal)                                  //If no color exists
+                    continue;                               //move onto the next element
+               if(colorList.indexOf(rgbVal.input) == -1)    //Avoids multiple entries
+                    colorList.push(rgbVal.input);           //If there are multiple elements using the same color,
+          }                                                 //It would be unneccessary to have multiple elements in a list
+     }
+     return colorList;
 }
 /*
 Step 4: Change each color by percent based values & threshold
@@ -65,7 +57,6 @@ Step 4: Change each color by percent based values & threshold
 	}else if(originalRGBValues < 77){
 		newRGBValues = originalRGBValues / 0.45;
 	}
-*/
-/*
+*//*
 Step 5: Inject new colors into CSS
 */
